8dd71a87f11a651ef2a9b5ea31e4ed39e69c3e50
GitHub Action <action@github.com>
2021-11-11 00:03:10 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/manaigrnamzn/intellijplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.manaigrnamzn.intellijplugintemplate.services
+
+import com.github.manaigrnamzn.intellijplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
